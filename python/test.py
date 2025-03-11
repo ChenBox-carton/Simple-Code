df766093d010d82ab2ceb3c14d102cd76c56f29f
chenbox <a339686214@gmail.com>
2025-03-11 13:35:56 +0800

    T003

--- python/test.py
+++ python/test.py
@@ -1,6 +1,21 @@
 import requests
 import json
 
-name = input("請輸入要查詢的魚貨名稱:")
-req = requests.get('https://data.moa.gov.tw/Service/OpenData/FromM/AquaticTransData.aspx')
-data = json.loads(req.text)
+name = input('請輸入要查詢的魚貨名稱:')
+
+try:
+  req = requests.get('https://data.moa.gov.tw/Service/OpenData/FromM/AquaticTransData.aspx')
+except:
+  pass
+
+datas = json.loads(req.text)
+
+for data in datas:
+  if data is not None and name in data['魚貨名稱']:
+    print(f"交易日期: {data['交易日期']}")
+    print(f"魚貨名稱: {data['魚貨名稱']}")
+    print(f"市場名稱: {data['市場名稱']}")
+    print(f"交易量: {data['交易量']}")
+    print(f"平均價: {data['平均價']}\n")
+  
+  
