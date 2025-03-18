42b967f188e13b9b88a6401acd418bf05bc0da4c
Chen Box <72123708+ChenBox-carton@users.noreply.github.com>
2025-03-18 10:05:41 +0800

    Update S004.py

--- python/S004.py
+++ python/S004.py
@@ -12,10 +12,22 @@ title = soup.title
 print(title.getText())
 print(title.getText(), file = note)
 
-select = soup.select('div.wrapper img')
+select = soup.select('div.wrapper ul')
 
-for i in select:
-    print(i['src'], i.text)
-    print(i['src'], i.text, file = note)
+# 第一組套餐的圖片在 wrapper class 裡面被註解掉了，並不能從select裡搜索到。
+# 真實圖片資料在 class="bb690" 的 img 可預先輸出不進入迴圈
+
+first_img = soup.select_one('img.bb690')
+img_src = first_img['src'] if first_img else "None"
+print(img_src, file = note)
+
+for data in select:
+    img = data.find('img')
+    text = data.text
+    img_src = img['src'] if img else "None"
+    # 可從上方的 img_src 的輸出結果看出，第一組套餐的圖片資料被註解掉了，所以會是 None
+
+    print(img_src, text)
+    print(img_src, text, file = note)
 
 note.close()
