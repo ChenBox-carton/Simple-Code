502bb3eab9ff469858d999ac13189b61de21e5b8
Chen Box <72123708+ChenBox-carton@users.noreply.github.com>
2025-03-28 14:52:13 +0800

    Update and rename T004.py to S005.py

--- python/S005.py
+++ python/S005.py
@@ -1,30 +1,39 @@
+#%%
 import pdfplumber
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
+
+# 中文字形
+plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']   
+plt.rcParams['axes.unicode_minus'] = False
+
 
 # 輸入要查詢的名字
-allName = [input(f'請輸入要查詢人名{i+1}：') for i in range(4)]
+allName = [input(f'請輸入要查詢人名{i+1}：') for i in range(3)]
 
 # 初始化計數
-allSum = [0] * 4
+allSum = [0] * 3
 
 # 開啟 PDF 並計算出現次數
 with pdfplumber.open('T004.pdf') as pdf:
     for page in pdf.pages:
         text = page.extract_text()
         if text:
-            for i in range(4):
+            for i in range(3):
                 allSum[i] += text.count(allName[i])
 
 # 顯示統計結果
-for i in range(4):
+for i in range(3):
     print(f"{allName[i]} 共出現 {allSum[i]} 次")
 
 # 繪製長條圖
 x = np.arange(len(allName))
-plt.bar(x, allSum, color=['red', 'green', 'blue', 'yellow'])
+plt.bar(x, allSum, color=['red', 'green', 'blue'])
 plt.xticks(x, allName)
 plt.xlabel('Name')
 plt.ylabel('Count')
-plt.title('T004 - 名字出現次數')
+plt.title('名字出現次數')
 plt.show()
+
+# %%
